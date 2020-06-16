d12d913903b2285214ecda3b0b988932d43a2cbf
mikanmatsuri <natsufuji83m@gmail.com>
2020-06-16 15:58:58 +0900

    #61 指摘箇所を修正した。その2

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@
 window.onload = function(){
     //変数、現在の言語に日本語を追加する
     var current_language = "jp";
-    //switch-languageというIDを取得して、それがクリックされた時
+    //switch-languageというId名がついている要素を一括取得して、それがクリックされた時
     document.getElementById("switch-language").onclick = function(){
         //text-enというクラス名がついている要素をすべて一括取得し、変数en_textsに入れる
         var en_texts = document.getElementsByClassName("text-en");
-        //以下波カッコに含まれている部分を繰り返す。何回繰り返すか。０からtext-enというクラス
+        ////【①「今、何回目の繰り返し処理中か」をカウントするための変数定義　②何回繰り返し処理をするか　③カウンタを1ずつ加算する】を繰り返す
         for(var i = 0; i < en_texts.length; i++){
             //もしも、現在の言語が日本語だったら
             if(current_language === "jp"){
-                //ｃｓｓに記載している英語表記のdisplayをブロックにして、クラス名en_textsを記載している箇所の英語を表示させる
+                //ｃｓｓに記載している英語表記のdisplayをブロックにして、変数en_textsを記載している箇所の英語を表示させる
                 en_texts[i].style.display = "block";
                 //そうでないなら（現在の言語が英語なら）
             } else {
-                //クラス名で記載したen_textsの箇所を、英語を表示しない
+                //ｃｓｓに記載している英語表記のdisplayをnoneにして、変数en_texts箇所の英語を表示しない
                 en_texts[i].style.display = "none";
             }
         }
@@ -26,11 +26,11 @@ window.onload = function(){
         for(var i = 0; i < jp_texts.length; i++){
             //もしも、現在の言語が日本語だったら
             if(current_language === "jp"){
-                //ｃｓｓに記載している日本語表記のdisplayをnoneにして、クラス名jp_textsを記載している箇所の日本語を表示しない
+                //ｃｓｓに記載している日本語表記のdisplayをnoneにして、変数jp_textsを記載している箇所の日本語を表示しない
                 jp_texts[i].style.display = "none";
             //日本語以外なら
             } else {
-                //ｃｓｓに記載している日本語表記のdisplayをブロックにして、クラス名jp_textsを記載している箇所の日本語を表示する
+                //ｃｓｓに記載している日本語表記のdisplayをブロックにして、変数jp_textsを記載している箇所の日本語を表示する
                 jp_texts[i].style.display = "block";
             }
         }
