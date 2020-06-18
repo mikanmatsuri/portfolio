c392cbf0983c9da932a8896765c48735dcd436c0
mikanmatsuri <natsufuji83m@gmail.com>
2020-06-18 13:46:52 +0900

    #61 JSで言語切り替えをスムーズに行えるようにした

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 // ページがダウンロードされるのを待つ
 window.onload = function(){
-    //変数、現在の言語に日本語を追加する
-    var current_language = "jp";
-    //switch-languageというId名がついている要素を一括取得して、それがクリックされた時
-    document.getElementById("switch-language").onclick = function(){
+
+    var showLanguage = function(current_language){
+        console.log(current_language);
         //text-enというクラス名がついている要素をすべて一括取得し、変数en_textsに入れる
         var en_texts = document.getElementsByClassName("text-en");
         ////【①「今、何回目の繰り返し処理中か」をカウントするための変数定義　②何回繰り返し処理をするか　③カウンタを1ずつ加算する】を繰り返す
@@ -11,11 +10,11 @@ window.onload = function(){
             //もしも、現在の言語が日本語だったら
             if(current_language === "jp"){
                 //ｃｓｓに記載している英語表記のdisplayをブロックにして、変数en_textsの要素の中身である英語を表示させる
-                en_texts[i].style.display = "block";
+                en_texts[i].style.display = "none";
                 //そうでないなら（現在の言語が英語なら）
             } else {
                 //ｃｓｓに記載している英語表記のdisplayをnoneにして、変数en_textsの要素の中身である英語を表示しない
-                en_texts[i].style.display = "none";
+                en_texts[i].style.display = "block";
             }
         }
         //text-enというクラス名がついている要素をすべて一括取得し、変数jp_textsに入れる
@@ -26,23 +25,48 @@ window.onload = function(){
             //もしも、現在の言語が日本語だったら
             if(current_language === "jp"){
                 //ｃｓｓに記載している日本語表記のdisplayをnoneにして、変数jp_textsの要素の中身である日本語を表示しない
-                jp_texts[i].style.display = "none";
-            //日本語以外なら
+                jp_texts[i].style.display = "block";
+                //日本語以外なら
             } else {
                 //ｃｓｓに記載している日本語表記のdisplayをブロックにして、要素の中身である日本語を表示する
-                jp_texts[i].style.display = "block";
+                jp_texts[i].style.display = "none";
             }
         }
+    };
+
+    var changeLanguage = function(){
         //もしも現在の言語が日本語と等しいなら
         if(current_language === "jp"){
             //現在の言語を英語にする
             current_language = "en";
-        //現在の言語が日本語以外なら
+            //現在の言語が日本語以外なら
         } else {
             //現在の言語を日本語にする
             current_language = "jp";
         }
+        localStorage.setItem("lang", current_language);
+        showLanguage(current_language);
+    };
+
+    //変数、現在の言語に日本語を追加する
+    var current_language = "jp";
+    //ローカルストレージに既に言語が入っていた場合
+    //つまりユーザーが既に言語を選択している場合
+    if(localStorage.getItem("lang")){
+        //現在の言語にローカルストレージに入っている言語の値を付与する
+        current_language = localStorage.getItem("lang");
+    //ローカルストレージに既に言語が入っていなかった場合
+    //つまりユーザーが言語を選択していない場合
+    } else {
+        //日本語を表示する
+        current_language = "jp";
     }
+
+    showLanguage(current_language);
+
+    //switch-languageというId名がついている要素を一括取得して、それがクリックされた時
+    document.getElementById("switch-language").onclick = changeLanguage;
+
 
     /*
     //ページの「予約メール」欄以外のものがクリックされたら、
